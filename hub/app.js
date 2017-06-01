1b3a6d9a21a8a00689016f16fd8938c0eba6051d
nelopauselli <nelopauselli@gmail.com>
2017-06-01 18:30:20 -0300

    hub

---
+++ hub/app.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var storage = require('./storage');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function (req, res) {
+	res.send("Hello, I'm an IoT hub");
+});
+
+app.get('/list', function (req, res) {
+	storage.listEvents(function (err, events) {
+		if (!err)
+			res.json(events);
+		else
+			res.sendStatus(500);
+	});
+});
+
+app.get('/count/:event', function (req, res) {
+	console.log(req.params.event);
+	storage.countEvents(req.params.event, function (err, events) {
+		res.json(events);
+	});
+});
+
+app.get('/temperature', function (req, res) {
+	storage.listEventsWithTemperature(function (err, events) {
+		res.json(events);
+	});
+});
+
+app.post('/', function (req, res) {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	var message = req.body;
+
+	console.log("Incomming message from " + ip + " to hub. Body: " + JSON.stringify(message));
+
+	storage.insert(message, function (err) {
+		if (!err)
+			res.sendStatus(200);
+		else
+			res.sendStatus(500);
+	});
+});
+
+app.get('*', function (req, res) {
+	res.status(404).send('what???');
+});
+
+app.use(function (err, req, res, next) {
+	console.error(err.stack);
+	res.status(500).send('Something broke!');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+	console.log('Example app listening on port ' + port + '!');
+});
---
+++ hub/storage.js
@@ -0,0 +1,92 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var mongo_ip = process.env.MONGODB_PORT_27017_TCP_ADDR || "192.168.1.116";
+var mongo_port = process.env.MONGODB_PORT_27017_TCP_PORT || "27017";
+var url = 'mongodb://' + mongo_ip + ':' + mongo_port + '/iot';
+
+console.log(url);
+
+module.exports = {
+	listEvents: function (callback) {
+		MongoClient.connect(url, function (err, db) {
+			assert.equal(null, err);
+			console.log("Connected correctly to server.");
+
+			var docs = [];
+			var cursor = db.collection('events').find();
+			cursor.each(function (err, doc) {
+				assert.equal(err, null);
+				if (doc != null) {
+					docs.push(doc);
+				} else {
+					db.close();
+					callback(null, docs);
+				}
+			});
+		});
+	},
+
+	listEventsWithTemperature: function (callback) {
+		MongoClient.connect(url, function (err, db) {
+			assert.equal(null, err);
+			console.log("Connected correctly to server.");
+
+			var docs = [];
+			var cursor = db.collection('events').find({ "temperature": { $exists: true } });
+			cursor.each(function (err, doc) {
+				assert.equal(err, null);
+				if (doc != null) {
+					docs.push(doc);
+				} else {
+					db.close();
+					callback(null, docs);
+				}
+			});
+		});
+	},
+
+	countEvents: function (eventName, callback) {
+		MongoClient.connect(url, function (err, db) {
+			assert.equal(null, err);
+			console.log("Connected correctly to server.");
+
+			var docs = [];
+			var query = {};
+
+			var criteria = {};
+			criteria[eventName] = { $exists: true };
+
+			var cursor = db.collection('events').aggregate(
+				[
+					{ $match: criteria },
+					{ $count: "count" }
+				]
+			);
+
+			cursor.each(function (err, doc) {
+				assert.equal(err, null);
+				if (doc != null) {
+					docs.push(doc);
+				} else {
+					db.close();
+					callback(null, docs);
+				}
+			});
+		});
+	},
+	insert: function (event, callback) {
+		MongoClient.connect(url, function (err, db) {
+			assert.equal(null, err);
+			console.log("Connected correctly to server.");
+			event.moment = new Date();
+			
+			db.collection('events').insertOne(event, function (err, result) {
+				assert.equal(err, null);
+				console.log("Inserted a document into the events collection.");
+				db.close();
+				callback(err);
+			});
+		});
+	}
+}
