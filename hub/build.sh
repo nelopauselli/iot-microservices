0116ec5f6a1e79d9ab636ca2075d3953564be936
Nelo Pauselli <nelopauselli@gmail.com>
2017-06-14 17:59:56 -0300

    balancer

--- hub/build.sh
+++ hub/build.sh
@@ -3,5 +3,5 @@ sudo docker stop hub
 sudo docker container rm hub
 sudo docker image rm hub
 sudo docker build -t hub .
-sudo docker run -p 8081:3000 -d -it --link mongo:mongodb --name hub hub
+sudo docker run -p 3000:3000 -d -it --link mongo:mongodb --name hub hub
 
