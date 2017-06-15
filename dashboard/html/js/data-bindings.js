c41dacf90f42c362fa079938a1a4ae26ab26489f
nelopauselli <nelopauselli@gmail.com>
2017-06-15 00:55:54 -0300

    terminando de quitar baseUrl

--- dashboard/html/js/data-bindings.js
+++ dashboard/html/js/data-bindings.js
@@ -5,7 +5,7 @@
             target.each(function (index, element) {
                 var eventName = $(element).data("target");
 
-                $.getJSON(baseUrl + "/count/" + eventName, function (response) {
+                $.getJSON("/api/count/" + eventName, function (response) {
                     var value = response[0].count;
                     $(element).text(parseInt(value));
                 });
